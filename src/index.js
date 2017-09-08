fee571ded7e81641dd28630ac29269e759af5a6c
pryahin <pryahin.v@gmail.com>
2017-09-09 02:23:54 +0300

    Registration view

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import signUp from './views/registration/registration.xml';
+import Registration from './views/registration/registration';
+
+import Router from './modules/router/router';
+
 
 const main = document.getElementById('main');
 
@@ -6,32 +9,9 @@ const main = document.getElementById('main');
 main.style.background = `url(static/img/backgrounds/${Math.floor(Math.random() * 3)}.jpg) no-repeat center fixed`;
 main.style.backgroundSize = 'cover';
 
-const loadRegistration = () => {
-    main.innerHTML = signUp({ title: 'Наша' });
+const signUp = new Registration();
+signUp.createRegistration(main);
 
-    const inp = document.getElementsByClassName('reginput');
+const router = new Router();
+router.go('index.html');
 
-    Object.keys(inp).forEach(el => {
-        inp[el].addEventListener('focus', () => {
-            this.classList.remove('input-error');
-        }, false);
-    });
-
-    document.getElementById('registrationSubmit').addEventListener('click', () => {
-        let valid = true;
-        Object.keys(inp).forEach(el => {
-            if (inp[el].value === '') {
-                inp[el].classList.add('input-error');
-                valid = false;
-            } else {
-                inp[el].classList.remove('input-error');
-            }
-        });
-
-        if (valid) {
-            document.forms['registration-form'].submit();
-        }
-    });
-};
-
-loadRegistration();
--- src/views/registration/registration.js
+++ src/views/registration/registration.js
@@ -1,3 +1,31 @@
-/**
- * Created by vova on 09.09.17.
- */
+import signUp from './registration.xml';
+
+export default class Registration {
+    createRegistration(main) {
+        main.innerHTML = signUp({ title: 'Наша' });
+
+        const inp = main.getElementsByClassName('reginput');
+
+        Object.keys(inp).forEach(el => {
+            inp[el].addEventListener('focus', () => {
+                inp[el].classList.remove('input-error');
+            }, false);
+        });
+
+        main.getElementsByClassName('registrationSubmit')[0].addEventListener('click', () => {
+            let valid = true;
+            Object.keys(inp).forEach(el => {
+                if (inp[el].value === '') {
+                    inp[el].classList.add('input-error');
+                    valid = false;
+                } else {
+                    inp[el].classList.remove('input-error');
+                }
+            });
+
+            if (valid) {
+                main.forms['registration-form'].submit();
+            }
+        });
+    }
+}
