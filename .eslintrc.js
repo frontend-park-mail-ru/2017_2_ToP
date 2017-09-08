e2587c4dc54170111d0ae22009a91092fabe3986
pryahin <pryahin.v@gmail.com>
2017-09-08 17:22:55 +0300

    ESlint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,35 @@
 module.exports = {
-    "extends": "google"
+    "extends": "airbnb-base",
+    "plugins": [
+        "import"
+    ],
+    "env": {
+        "es6": true,
+        "browser": true
+    },
+    "rules": {
+        "no-underscore-dangle": 0,
+        "no-param-reassign": 0,
+        "no-lonely-if": 0,
+        "arrow-parens": ["error", "as-needed"],
+        "class-methods-use-this": 0,
+        "comma-dangle": [2, "never"],
+        "no-console": 0,
+        "no-useless-escape": 0,
+        "quotes": ["error", "single", {"allowTemplateLiterals": true}],
+        "import/no-unresolved": [2, {"commonjs": true, "amd": true}],
+        "prefer-template": "error",
+        "object-shorthand": "error",
+        "indent": ["error", 4, {"FunctionDeclaration": {"body": 1, "parameters": 2}, "SwitchCase": 1}],
+        "no-plusplus": 0,
+        "no-unused-expressions": ["error", {"allowShortCircuit": true}],
+        "no-shadow": 0,
+        "no-extra-bind": 0,
+        "no-useless-constructor": 0,
+        "no-unused-vars": 0,
+        "quote-props": 0,
+        "arrow-body-style": 0,
+        "padded-blocks": 0,
+        "operator-assignment": 0
+    }
 };
--- src/index.js
+++ src/index.js
@@ -1,38 +1,37 @@
 import signUp from './views/registration/registration.xml';
 
-const main = document.getElementById("main");
+const main = document.getElementById('main');
 
-//background
-main.style.background = 'url(static/img/backgrounds/' + Math.floor(Math.random() * 3) + '.jpg) no-repeat center fixed';
+// background
+main.style.background = `url(static/img/backgrounds/${Math.floor(Math.random() * 3)}.jpg) no-repeat center fixed`;
 main.style.backgroundSize = 'cover';
 
 const loadRegistration = () => {
-    main.innerHTML = signUp({title: "Наша"});
+    main.innerHTML = signUp({ title: 'Наша' });
 
-    const inp = document.getElementsByClassName("reginput");
+    const inp = document.getElementsByClassName('reginput');
 
     Object.keys(inp).forEach(el => {
-        inp[el].addEventListener("focus", function () {
-            this.classList.remove("input-error");
+        inp[el].addEventListener('focus', () => {
+            this.classList.remove('input-error');
         }, false);
     });
 
     document.getElementById('registrationSubmit').addEventListener('click', () => {
         let valid = true;
         Object.keys(inp).forEach(el => {
-            if (inp[el].value == "") {
-                inp[el].classList.add("input-error");
+            if (inp[el].value === '') {
+                inp[el].classList.add('input-error');
                 valid = false;
-            }
-            else {
-                inp[el].classList.remove("input-error");
+            } else {
+                inp[el].classList.remove('input-error');
             }
         });
 
         if (valid) {
-            document.forms["registration-form"].submit();
+            document.forms['registration-form'].submit();
         }
     });
-}
+};
 
 loadRegistration();
