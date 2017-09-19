1cdd333291e2a1b2547daa11397a06b8a9faf4e9
pryahin <pryahin.v@gmail.com>
2017-09-19 22:40:40 +0300

    Added _readystatechange function

--- src/modules/Transport/Transport.js
+++ src/modules/Transport/Transport.js
@@ -12,23 +12,26 @@ export default class Transport {
         xhr.open(method, url, true);
         xhr.withCredentials = true;
 
-        xhr.addEventListener('readystatechange', () => {
-            if (xhr.readyState !== XMLHttpRequest.DONE) {
-                return;
-            }
-
-            if (+xhr.status >= 400) {
-                return callback(xhr, null);
-            }
-
-            const response = JSON.parse(xhr.responseText);
-            callback(null, response);
-        }, false);
+        xhr.addEventListener('readystatechange', Transport._readystatechange.bind(this, xhr, callback), false);
 
         if (body) {
             xhr.setRequestHeader('Content-Type', 'application/json; charset=utf8');
         }
 
         xhr.send(JSON.stringify(body));
+    }
+
+    static _readystatechange(xhr, callback) {
+        console.log(xhr);
+        if (xhr.readyState !== XMLHttpRequest.DONE) {
+            return;
+        }
+
+        if (+xhr.status >= 400) {
+            return callback(xhr, null);
+        }
+
+        const response = JSON.parse(xhr.responseText);
+        callback(null, response);
     }
 }
