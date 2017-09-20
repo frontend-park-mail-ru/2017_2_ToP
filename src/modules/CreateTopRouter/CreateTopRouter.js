d74c0067052dfe503b102b7310a8163afdb3c6ac
couatl <toscharlina@yandex.ru>
2017-09-20 18:29:23 +0300

    Fix

--- src/modules/CreateTopRouter/CreateTopRouter.js
+++ src/modules/CreateTopRouter/CreateTopRouter.js
@@ -2,7 +2,6 @@ import Router from '../Router/Router';
 import {appendChilds} from '../../components/TopComponent/TopComponent';
 
 export default function CreateTopRouter(className, componentsRoutes, defaultComponents) {
-    const classObject = document.getElementsByClassName(className)[0];
     const router = new Router();
 
     appendChilds(className, defaultComponents);
