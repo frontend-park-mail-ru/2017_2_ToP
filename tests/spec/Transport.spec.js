162e9296836f20a39b7dfd74b82a6887ff6db55f
couatl <toscharlina@yandex.ru>
2017-09-25 16:09:01 +0300

    Tests

---
+++ tests/spec/Transport.spec.js
@@ -0,0 +1,59 @@
+import Transport from '../../src/modules/Transport/Transport';
+import 'whatwg-fetch';
+
+describe('Api tests', () => {
+    it('Logout error', (done) => {
+        Transport.get('logout')
+            .catch(response => {
+                expect(response.status).to.equal(401);
+                done();
+            });
+    });
+
+    it('Register', (done) => {
+        const obj = {
+            login: "test",
+            email: "test@apoj.ru",
+            password: "Password1"
+        };
+        obj['repeat-password'] = "Password1";
+        Transport.post('signup', obj)
+            .then(response => {
+                expect(response).to.have.property('id');
+                done();
+            })
+            .catch(response => {
+                expect(response.status).to.equal(400);
+                done();
+            });
+    });
+
+    it('Sign In', (done) => {
+        Transport.post('signin', {
+            login: "test",
+            password: "Password1"
+        })
+            .then(response => {
+                expect(response).to.have.property('id');
+                done();
+            })
+            .catch(response => {
+                response.json().then(json => {
+                    console.log(`${response.status}: ${response.statusText}\n${json.message}`);
+                });
+                done();
+            });
+    });
+
+    it('Current user', (done) => {
+        Transport.get('user')
+            .then(response => {
+                expect(response).to.have.property('id');
+                done();
+            })
+            .catch(response => {
+                expect(response.status).to.equal(401);
+                done();
+            });
+    });
+});
