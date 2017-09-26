b6487e3106f1b1f13d1b3acaad76a2c78ccae02c
couatl <toscharlina@yandex.ru>
2017-09-26 18:15:59 +0300

    Logout button

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,5 +1,6 @@
 import TopComponent from '../TopComponent/TopComponent';
 import Button from '../Button/Button';
+import UserService from '../../services/UserService/UserService';
 
 export default class Menu extends TopComponent {
     constructor(data) {
@@ -11,6 +12,26 @@ export default class Menu extends TopComponent {
             const button = new Button(el.text, el.url);
             this.append(button.render());
         });
+        this._logout();
         return this.getElement();
+    }
+
+    _logout() {
+        const main = this.getElement();
+        console.log(this.getElement());
+
+        main.getElementsByClassName('button')[0].addEventListener('click', () => {
+            if (this.getData().method === 'get') {
+                UserService.logout()
+                    .then(response => {
+                        window.router.go('/');
+                    })
+                    .catch(response => {
+                        response.json().then(json => {
+                            console.log(`${response.status}: ${response.statusText}\n${json.message}`);
+                        });
+                    });
+            }
+        });
     }
 }
