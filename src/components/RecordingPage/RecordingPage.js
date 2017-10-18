f7674d37006fd62676e8971d1442727d027ba513
Svoykina N <nadya.svojkina@gmail.com>
2017-10-18 03:21:07 +0300

    change RecordingPage
    Added game rules description

--- src/components/RecordingPage/RecordingPage.js
+++ src/components/RecordingPage/RecordingPage.js
@@ -2,24 +2,34 @@ import TopComponent from '../TopComponent/TopComponent';
 import GameText from '../GameText/GameText';
 import AudioPlayer from '../AudioPlayer/AudioPlayer';
 import RecordPlayer from '../RecordPlayer/RecordPlayer';
+import Button from '../Button/Button';
 
 export default class RecordingPage extends TopComponent {
     constructor(data) {
-        super('div', { 'class': 'recording-page' }, data);
+        super('div', {'class': 'recording-page'}, data);
     }
 
     render() {
-        this.components = [new GameText({
-            text: 'Добро пожаловать!'
-        }),
-        new GameText({
-            text: 'Прослушайте фрагмент и когда будете готовы, начните его запись'
-        }),
-        new AudioPlayer(this.getData()),
-        new RecordPlayer()];
+        const audio = new AudioPlayer(this.getData());
+
+        this.components = [
+            new GameText({
+                text: 'Прослушайте фрагмент песни и когда будете готовы, начните его запись.'
+            }),
+            audio,
+            new GameText({
+                text: 'Постарайтесь уложиться в 10 секунд.'
+            }),
+            new RecordPlayer({
+                audioPlayer: audio
+            }),
+            new Button('Отправить', '')
+        ];
+
         this.components.forEach(element => {
             this.append(element.render());
         });
+
         return this.getElement();
     }
 }
