cad369e16593e3d803acce02f6863155c1f1d49d
Svoykina N <nadya.svojkina@gmail.com>
2017-10-18 05:38:34 +0300

    structuring RecordingPage

--- src/components/RecordingPage/RecordingPage.js
+++ src/components/RecordingPage/RecordingPage.js
@@ -4,32 +4,67 @@ import AudioPlayer from '../AudioPlayer/AudioPlayer';
 import RecordPlayer from '../RecordPlayer/RecordPlayer';
 import Button from '../Button/Button';
 
+const textData = {
+    texts: [
+        'Прослушайте фрагмент песни и когда будете готовы, начните его запись.',
+        'Постарайтесь уложиться в 10 секунд.',
+    ],
+    buttons: [
+        {
+            url: '',
+            text: 'Отправить!'
+        }
+    ]
+};
+
 export default class RecordingPage extends TopComponent {
     constructor(data) {
         super('div', {'class': 'recording-page'}, data);
+        this._textData = textData;
+    }
+
+    getMusicURL() {
+        return this._components[3].getMusicURL();
+    }
+
+    getSubmitButton() {
+        return this._components[4].getElement();
     }
 
     render() {
-        const audio = new AudioPlayer(this.getData());
-
-        this.components = [
+        this._components = [
             new GameText({
-                text: 'Прослушайте фрагмент песни и когда будете готовы, начните его запись.'
+                text: this._textData.texts[0]
             }),
-            audio,
+            new AudioPlayer(this.getData()),
             new GameText({
-                text: 'Постарайтесь уложиться в 10 секунд.'
+                text: this._textData.texts[1]
             }),
-            new RecordPlayer({
-                audioPlayer: audio
-            }),
-            new Button('Отправить', '')
+            new RecordPlayer(),
+            new Button(
+                this._textData.buttons[0].text,
+                this._textData.buttons[0].url
+            )
         ];
 
-        this.components.forEach(element => {
+        this._components.forEach(element => {
             this.append(element.render());
         });
 
         return this.getElement();
+    }
+
+    show() {
+        if (this._components) {
+            this._components.forEach(element => element.show());
+        } else {
+            this.render();
+        }
+    }
+
+    hide() {
+        if (this._components) {
+            this._components.forEach(element => element.hide());
+        }
     }
 }
