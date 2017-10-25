c29a3010e1c5a582c48ab615d22b554c628ec857
Vladimir Pryahin <pryahin.v@gmail.com>
2017-10-25 15:23:45 +0300

    EndingPage

---
+++ src/components/Game/EndingPage/EndingPage.js
@@ -0,0 +1,36 @@
+import TopComponent from '../../TopComponent/TopComponent';
+import GameText from '../GameText/GameText';
+import VideoPlayer from '../VideoPlayer/VideoPlayer';
+import UserService from '../../../services/UserService/UserService';
+
+import './EndingPage.scss';
+
+export default class ListeningPage extends TopComponent {
+    constructor(data) {
+        super('div', {'class': 'ending-page'}, data);
+    }
+
+    render() {
+        if (this.getData().isWin) {
+            this._components = [
+                new GameText({
+                    text: 'Победа!',
+                    title: true
+                }),
+                new VideoPlayer({
+                    src: '/static/video/win.mp4',
+                    type: 'video/mp4'
+                }),
+                new GameText({
+                    text: `Ваш новый счет ${UserService.getScore('single')}`
+                })
+            ];
+        }
+
+        this._components.forEach(element => {
+            this.append(element.render());
+        });
+
+        return this.getElement();
+    }
+}
