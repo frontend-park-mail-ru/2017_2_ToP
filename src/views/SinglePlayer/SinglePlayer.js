b8b115bea1d28a34753cb887b604df978bda77ed
Svoykina N <nadya.svojkina@gmail.com>
2017-10-26 15:12:08 +0300

    + before game menu

--- src/views/SinglePlayer/SinglePlayer.js
+++ src/views/SinglePlayer/SinglePlayer.js
@@ -3,19 +3,38 @@ import TopComponent from '../../components/TopComponent/TopComponent';
 import RecordingPage from '../../components/Game/Stages/Recording/Recording';
 import ListeningPage from '../../components/Game/Stages/Listening/Listening';
 import EndingPage from '../../components/Game/Stages/Ending/Ending';
+import Menu from '../../components/Menu/Menu';
 
 import UserService from '../../services/UserService/UserService';
 import router from '../../modules/Router/Router';
 
+const preGameData = {
+    buttons: [
+        {
+            text: 'Продолжить',
+            url: ''
+        },
+        {
+            text: 'Начать новую',
+            url: ''
+        }
+    ]
+};
+
 export default class SinglePlayer extends TopComponent {
     constructor() {
-        super('div', {});
+        super('div', {}, preGameData);
     }
 
     show() {
         if (!(UserService.isLoggedIn())) {
             router.go('/');
-        } else if (this._endGame) {
+            return;
+        }
+
+        this._initPreGame();
+
+        if (this._endGame) {
             this._components.forEach(element => element.remove());
             this._components = [];
             this.build();
@@ -42,6 +61,31 @@ export default class SinglePlayer extends TopComponent {
         ];
         this._components[0].renderTo('content');
         this._initRecordingPage();
+    }
+
+    _initPreGame() {
+        if (this._endGame || !this._components[0]) {
+            return;
+        }
+
+        this._previousPage = this._components[0];
+
+        this._components = [new Menu(this.getData())];
+        const menu = this._components[0];
+        menu.renderTo('content');
+
+        const buttons = menu.getElement().getElementsByClassName('button');
+        buttons[0].addEventListener('click', () => {
+            menu.remove();
+            this._components = [this._previousPage];
+            this._components.forEach(element => element.show());
+        });
+        buttons[1].addEventListener('click', () => {
+            menu.remove();
+            this._previousPage.remove();
+            this._components = [];
+            this.build();
+        });
     }
 
     _initRecordingPage() {
