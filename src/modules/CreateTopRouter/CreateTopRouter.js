c27497431a59acc65d8af7121c55f40e74210b64
Svoykina N <nadya.svojkina@gmail.com>
2017-10-28 07:12:23 +0300

    setting after reload and closing

--- src/modules/CreateTopRouter/CreateTopRouter.js
+++ src/modules/CreateTopRouter/CreateTopRouter.js
@@ -1,9 +1,13 @@
 import router from '../Router/Router';
+import Theming from '../Theming/Theming';
 import {appendChilds} from '../../components/TopComponent/TopComponent';
 import UserService from '../../services/UserService/UserService';
 import Loading from '../../components/Loading/Loading';
 
 export default function CreateTopRouter(className, componentsRoutes, defaultComponents) {
+    const theming = new Theming();
+    theming.setTheme();
+
     appendChilds(className, defaultComponents);
     componentsRoutes.forEach(route => router.use(route.path, route.component));
     router.connectRouting(window);
