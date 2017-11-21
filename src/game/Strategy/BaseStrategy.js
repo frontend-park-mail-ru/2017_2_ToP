78791e6979e9f0d4381369a61fcc2d0fb1b1240e
Vladimir Pryahin <pryahin.v@gmail.com>
2017-11-21 23:33:59 +0300

    Base strategy

---
+++ src/game/Strategy/BaseStrategy.js
@@ -0,0 +1,26 @@
+import GameScene from '../GameScene/GameScene';
+
+export default class BaseStrategy {
+    constructor(path) {
+        const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+        this._socket = new WebSocket(`${protocol}//${path}`);
+
+        this.stage = null;
+        this.stages = [];
+        this.scene = new GameScene();
+    }
+
+    next() {
+        if (this.stages.length === 0) {
+            this.finish();
+            return;
+        }
+
+        this.stage = this.stages.shift();
+        this.scene.setStage(this.stage);
+    }
+
+    finish() {
+        this.scene.destroy();
+    }
+}
