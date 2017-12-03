7065fd5da3ecf02f8c3db917ccce2cd3d0e52add
Vladimir Pryahin <pryahin.v@gmail.com>
2017-12-03 16:17:13 +0300

    Multiplayer constnts

---
+++ src/game/Constants/Multiplayer.js
@@ -0,0 +1,4 @@
+export const RECORDNG_MESSAGE = 'Второй игрок записывает';
+export const READY_MESSAGE1 = 'Ваша запись перевернута';
+export const READY_MESSAGE2 = 'Можете прослушать, что вышло у второго игрока';
+export const GOT_RESULT = 'Подведены итоги';
