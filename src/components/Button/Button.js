3558037de8d0a45b64f264de14ec748d0eb284ff
Vladimir Pryahin <pryahin.v@gmail.com>
2017-12-03 17:13:20 +0300

    Changed button constructor

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,7 @@
 import TopComponent from '../TopComponent/TopComponent';
 
 export default class Button extends TopComponent {
-    constructor(text, url = null, className = 'button') {
+    constructor({text, url = null, class: className = 'button'}) {
         super('div', {'class': className, 'data-url': url});
         this.setText(text);
     }
