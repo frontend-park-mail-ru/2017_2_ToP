036b5f6d9c3404b906f0dc3d59c21f82ebc9f793
Vladimir Pryahin <pryahin.v@gmail.com>
2017-12-03 23:56:10 +0300

    Refactor

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,5 @@
 import TopComponent from '../TopComponent/TopComponent';
 
-import './Button.scss';
-
 export default class Button extends TopComponent {
     constructor({text, url = '', class: className = 'button'}) {
         super('div', {'class': className, 'data-url': url});
