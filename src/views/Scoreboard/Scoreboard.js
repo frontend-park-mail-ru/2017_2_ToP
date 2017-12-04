1c8e76368862f8a1b16524cc6607531c763c3b97
Vladimir Pryahin <pryahin.v@gmail.com>
2017-12-04 17:30:37 +0300

    scoreboard with new table

--- src/views/Scoreboard/Scoreboard.js
+++ src/views/Scoreboard/Scoreboard.js
@@ -7,15 +7,14 @@ import Button from '../../components/Button/Button';
 
 const TMP_DATA = {
     head: [
-        '#',
         'Имя',
         'Одиночная игра',
         'Многопользовательская'
     ],
     body: [
-        ['1', 'test', 1000, 1000],
-        ['2', 'test2', 500, 500],
-        ['3', 'test3', 300, 300]
+        ['test', 1000, 1000],
+        ['test2', 500, 500],
+        ['test3', 300, 300]
     ]
 };
 
@@ -35,33 +34,37 @@ export default class Scoreboard extends TopComponent {
     }
 
     build() {
-        this.table = new Table(TMP_DATA);
+        this.table = new Table(TMP_DATA, 'content__scoreboard__table');
         this._buildMoreButton();
 
         this._components = [
             this.table,
+            this.moreButton,
             new BackButton()
         ];
         this._components.forEach(element => this.append(element.render()));
         this.renderTo('content');
     }
 
-    _buildMoreButton() {
-        if (!this.isFull) {
-            const _table = this.table.getElement().querySelector('table');
-            if (!this.moreButton) {
-                this._initButton();
-            }
-            this.moreButton.getElement().setAttribute('class', _table.rows.length % 2 ? 'button-more_odd' : 'button-more_even');
-
-            this.table.append(this.moreButton.getElement());
-        }
+    _rerenderMoreButton() {
+        const _table = this.table.getElement();
+        this.moreButton.getElement().setAttribute('class', _table.rows.length % 2 ? 'button-more_odd' : 'button-more_even');
     }
 
     _initButton() {
         this.moreButton = new Button({text: '. . .'});
         this.moreButton.getElement().addEventListener('click', () => {
-            console.log('Тут будет получение дынных');
+            this.table.addRow(TMP_DATA.body);
+            this._rerenderMoreButton();
         });
+    }
+
+    _buildMoreButton() {
+        if (!this.isFull) {
+            if (!this.moreButton) {
+                this._initButton();
+            }
+            this._rerenderMoreButton();
+        }
     }
 }
