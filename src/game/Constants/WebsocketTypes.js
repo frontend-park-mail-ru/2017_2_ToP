3099861915a173735205c99aba007b0aa5494535
Vladimir Pryahin <pryahin.v@gmail.com>
2017-12-07 21:31:01 +0300

    Changed messages logic

--- src/game/Constants/WebsocketTypes.js
+++ src/game/Constants/WebsocketTypes.js
@@ -1,5 +1,5 @@
 export const PREGAME_DATA = 'preGameData';
 export const RECORDING = 'Recording';
+export const SECOND_RECORDING = 'SecondRecording';
 export const LISTENING = 'Listening';
-export const SECOND_LISTENING = 'SecondListening';
 export const RESULT = 'Result';
