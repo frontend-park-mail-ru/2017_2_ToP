45bede0c1c6e126fb38218962f93dd5f5995fcb1
Vladimir Pryahin <pryahin.v@gmail.com>
2017-12-08 23:38:59 +0300

    -fetch

--- src/ServiceWorker.js
+++ src/ServiceWorker.js
@@ -22,21 +22,3 @@ this.addEventListener('install', event => {
             ]);
         }));
 });
-
-this.addEventListener('fetch', event => {
-    event.respondWith(caches.match(event.request)
-        .then(cachedResponse => {
-            return cachedResponse || fetch(event.request.clone())
-                .then(response => {
-                    const responseClone = response.clone();
-
-                    caches.open('MY_CACHE')
-                        .then(cache => {
-                            cache.put(event.request, responseClone)
-                                .catch(err => {});
-                        });
-
-                    return response;
-                });
-        }));
-});
