52c3212894a3e2f3b0455cab22e19cfaa8c548da
Vladimir Pryahin <pryahin.v@gmail.com>
2017-12-08 18:23:24 +0300

    Fix

--- src/components/Game/Stages/PreGame/PreGame.js
+++ src/components/Game/Stages/PreGame/PreGame.js
@@ -1,7 +1,7 @@
 import TopComponent from '../../../TopComponent/TopComponent';
 import Menu from '../../../Menu/Menu';
 
-import {NEWGAME_BUTTON, COUNTINUE_BUTTON} from '../../../../constants/Stages';
+import {NEWGAME_BUTTON, CONTINUE_BUTTON} from '../../../../constants/Stages';
 
 const preGameData = {
     buttons: [
